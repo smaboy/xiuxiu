65033a641649e79d2a1b4e21e6ed3687ee50b4c1
Smaboy <18550019225@163.com>
2018-09-19 17:13:35 +0800

    优化欢迎界面UI

--- down/xiu2xiu/src/main/java/com/smaboy/pc120/xiuxiu/c/activity/SplashActivity.java
+++ down/xiu2xiu/src/main/java/com/smaboy/pc120/xiuxiu/c/activity/SplashActivity.java
@@ -6,6 +6,9 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.smaboy.pc120.xiuxiu.R;
@@ -30,6 +33,8 @@ public class SplashActivity extends BaseActivity {
     TextView tvAppName;
     @BindView(R.id.tv_version_name)
     TextView tvVersionName;
+    @BindView(R.id.iv_loading)
+    ImageView ivLoading;
     private Handler handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message message) {
@@ -41,12 +46,18 @@ public class SplashActivity extends BaseActivity {
                 case 1:
 
                     startActivity(new Intent(SplashActivity.this, LoginActivity_1_0.class));
+                    if(rotateAnimation!=null) {
+                        rotateAnimation.cancel();
+                        rotateAnimation=null;
+                    }
                     finish();
+
                     break;
             }
             return false;
         }
     });
+    private RotateAnimation rotateAnimation;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,7 +70,7 @@ public class SplashActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         //设置隐藏状态栏
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
         initData();
@@ -70,6 +81,16 @@ public class SplashActivity extends BaseActivity {
         //设置app名称和版本号
         tvAppName.setText(R.string.app_name);
         tvVersionName.setText(PackageHelpUtils.getVersionName(context));
+
+        //设置加载图标的动画
+        rotateAnimation=new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        rotateAnimation.setRepeatMode(RotateAnimation.RESTART);//从头开始
+        rotateAnimation.setRepeatCount(RotateAnimation.INFINITE);//无限循环
+        rotateAnimation.setFillAfter(true); // 设置保持动画最后的状态
+        rotateAnimation.setDuration(3000); // 设置动画时间
+        rotateAnimation.setInterpolator(new LinearInterpolator()); // 设置插入器
+        ivLoading.setAnimation(rotateAnimation);
+
 
     }
 
